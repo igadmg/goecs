544613d0891b3b4f08c730c0e2683f41599c1123
Igor Karatayev <igor.cwer@gmail.com>
2025-03-05 00:22:21 +0300

    generate SetA for a tagged fields

--- cmd/ecs.GeneratorEcs.generateEntity.go.el
+++ cmd/ecs.GeneratorEcs.generateEntity.go.el
@@ -12,14 +12,24 @@ func (g *GeneratorEcs) genAs(wr io.Writer, e *Type) {
 			for af := range fet.AsComponentsSeq() {
 				if af.IsEcsRef() {
 ?>
+
 func (e *<?= e.Name ?>) <?= af.GetA() ?>() *<?= af.GetType().GetName() ?> {
 	return e.<?= f.GetName() ?>.<?= af.GetName() ?>.Get()
 }
+
+//func (e *<?= e.Name ?>) Set<?= af.GetA() ?>(v <?= af.GetTypeName() ?>) {
+//	e.<?= f.GetName() ?>.<?= af.GetName() ?> = v
+//}
 <?
 				} else {
 ?>
+
 func (e *<?= e.Name ?>) <?= af.GetA() ?>() <?= af.GetTypeName() ?> {
 	return e.<?= f.GetName() ?>.<?= af.GetName() ?>
+}
+
+func (e *<?= e.Name ?>) Set<?= af.GetA() ?>(v <?= af.GetTypeName() ?>) {
+	e.<?= f.GetName() ?>.<?= af.GetName() ?> = v
 }
 <?					
 				}
