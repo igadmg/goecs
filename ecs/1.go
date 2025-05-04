518428af535b123723b2f7e78348088fc4585c72
Igor Karatayev <igor.cwer@gmail.com>
2025-05-04 14:50:54 +0300

    fixed MetaTag

--- ecs/1.go
+++
@@ -1,5 +0,0 @@
-package ecs
-
-//go:generate go tool github.com/igadmg/goel
-
-type MetaTag struct{} // Uset to add tags to structs
--- ecs/ecs.go
+++ ecs/ecs.go
@@ -1,5 +1,7 @@
 package ecs
 
+type MetaTag struct{} // Used to add tags to structs
+
 //type Query[T any] struct {
 //	Age   uint64
 //	Items []T
